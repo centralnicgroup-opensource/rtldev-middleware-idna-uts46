8038a3bd8fb8539b17a74c47c21c8c74f2aed290
Kai Schwarz <kai.schwarz@centralnic.com>
2024-04-24 22:50:53 +0200

    build(npm): upgrade to eslint v9

---
+++ eslint.config.cjs
@@ -0,0 +1,28 @@
+const globals = require("globals");
+const recommended = require("@eslint/js").configs.recommended;
+
+module.exports = {
+  ...recommended,
+  languageOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    globals: {
+      ...globals.es6,
+      ...globals.node
+    },
+  },
+  ...{
+    files: [ 'test/*.js' ],
+    languageOptions: {
+      globals: {
+       ...globals.mocha,
+      }
+    }
+  },
+  ignores: [
+      "dist",
+      "test/index.bundle.js",
+      "test/index.mjs",
+      "test/mocha.css"
+  ]
+};  
